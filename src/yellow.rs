f665b1cb58113255ba79877e6b583a5a9be79e04
dcts <44790691+dcts@users.noreply.github.com>
2022-07-12 16:01:43 +0200

    :white_check_mark: test panic + improve styling

--- src/yellow.rs
+++ src/yellow.rs
@@ -49,18 +49,25 @@ pub fn adjacent_position_to_the_left(x: usize, y: usize) -> (usize, usize) {
     (0, 0)
 }
 
+
+
+
+
+
 /*
- * ADD UNIT TESTS
+ * UNIT TESTS
  */
 #[cfg(test)]
 mod tests {
     use crate::yellow::*;
+    
     #[test]
     fn sum_test() {
         assert_eq!(sum(1,4), 5);
         assert_eq!(sum(12,1200239), 1200251);
         assert_eq!(sum(0,0), 0);
     }
+    
     #[test]
     fn are_positions_equal_test() {
         // should be true
@@ -74,6 +81,7 @@ mod tests {
         assert_eq!(are_positions_equal(12,1,13,0), false); // both mismatch
         assert_eq!(are_positions_equal(1,0,12,3), false); // both mismatch
     }
+    
     #[test]
     fn is_position_inside_map_bounds_test() {
         // tests taken from description
@@ -92,12 +100,14 @@ mod tests {
         assert_eq!(is_position_inside_map_bounds(2,1,2,2), false);
         assert_eq!(is_position_inside_map_bounds(1,2,2,2), false);
     }
+    
     #[test]
     fn absolute_test() {
         assert_eq!(absolute(0), 0);
         assert_eq!(absolute(12), 12);
         assert_eq!(absolute(-4), 4);
     }
+    
     #[test]
     fn distance_test() {
         assert_eq!(distance(0,0,0,0), 0);
@@ -108,6 +118,7 @@ mod tests {
         assert_eq!(distance(1,5,5,1), 8);
         assert_eq!(distance(1,5,15,1), 18);
     }
+    
     #[test]
     fn adjacent_position_to_the_left_test() {
         // test taken from description
@@ -116,5 +127,12 @@ mod tests {
         assert_eq!(adjacent_position_to_the_left(5, 5), (4, 5));
         assert_eq!(adjacent_position_to_the_left(1, 1), (0, 1));
         assert_eq!(adjacent_position_to_the_left(1, 0), (0, 0));
+    }
+    
+    #[test]
+    #[should_panic]
+    fn adjacent_position_to_the_left_panic_test() {
+        // out of bounds should panic
+        adjacent_position_to_the_left(0, 0);
     }
 }
